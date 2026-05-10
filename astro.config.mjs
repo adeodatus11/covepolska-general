910798a32f59de69feefbb8ff27d67c7639b452d
mnajwer <demotyczny.rowerem_0x@icloud.com>
2026-05-10 16:48:39 +0200

    Fix GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 export default defineConfig({
-  site: "https://covepolska.pl",
+  site: isGitHubPages ? "https://adeodatus11.github.io" : "https://covepolska.pl",
+  base: isGitHubPages ? "/covepolska-general" : "/",
   integrations: [sitemap()],
   output: "static"
 });
