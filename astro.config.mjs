fbb859ec72c235939e2f53a12d38e9555d68300e
mnajwer <demotyczny.rowerem_0x@icloud.com>
2026-05-19 12:01:35 +0200

    Fix new domain links and footer logos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-const isGitHubPages = process.env.GITHUB_PAGES === "true";
-
 export default defineConfig({
-  site: isGitHubPages ? "https://adeodatus11.github.io" : "https://covepolska.pl",
-  base: isGitHubPages ? "/covepolska-general" : "/",
+  site: "http://new.covepolska.pl",
+  base: "/",
   integrations: [sitemap()],
   output: "static"
 });
