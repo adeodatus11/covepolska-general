6fada83e7ae7d281bcdfb858cd91de8465321b0a
mnajwer <demotyczny.rowerem_0x@icloud.com>
2026-07-22 16:57:17 +0200

    Use covepolska production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "http://new.covepolska.pl",
+  site: "https://covepolska.pl",
   base: "/",
   integrations: [sitemap()],
   output: "static"
